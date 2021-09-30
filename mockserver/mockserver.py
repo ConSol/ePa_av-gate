b089bba97a9ba7aa6341dce792e023077e162ccf
Norbert Ferchen <nferchen@consol.de>
2021-09-30 17:51:57 +0200

    add mockserver

---
+++ mockserver/mockserver.py
@@ -0,0 +1,17 @@
+from flask import Flask, make_response
+import io
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+  return "up and running"
+
+@app.route("/soap-api")
+def soap():
+  fn = open("./samples/retrievedocument-resp.xop", "br")
+  data = io.BytesIO(fn.read())
+  response = make_response(data)
+  response.headers.set('Content-Type', 'application/xop+xml; type="application/soap+xml"')
+  response.headers.set('Content-Transfer-Encoding', 'binary')
+  return response
