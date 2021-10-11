683b9c53431a8f66d55edb5255ab05dcefce1783
Norbert Ferchen <nferchen@consol.de>
2021-10-11 13:45:01 +0200

    add ini file an connector.sds

--- av_gate.py
+++ av_gate.py
@@ -1,3 +1,4 @@
+import configparser
 import email.parser
 import email.policy
 import io
@@ -7,56 +8,114 @@ import clamd
 import requests
 from flask import Flask, Response, request
 
-CLAMD_SOCKET = "/tmp/clamd.socket"
-UPSTREAM_SERVER = "https://kon-instanz1.titus.ti-dienste.de" # "https://127.0.0.1:5000"
-PROXY_SSL_CERT = "cert/ps_epa_consol_01.crt"
-PROXY_SSL_KEY = "cert/ps_epa_consol_01.key"
-SSL_VERIFY = True
+ALL_METHODS = [
+    "GET",
+    "HEAD",
+    "POST",
+    "PUT",
+    "DELETE",
+    "CONNECT",
+    "OPTIONS",
+    "TRACE",
+    "PATCH",
+]
 
-VIRUS_MSG = "Das Dokument ist mit einem Virus infiziert und wird nicht übertragen."
+config = configparser.ConfigParser()
+config.read("av_gate.ini")
 
-cd = clamd.ClamdUnixSocket(path=CLAMD_SOCKET)
+clamav = clamd.ClamdUnixSocket(path=config["config"]["clamd_socket"])
+
 reg_retrieve_document = re.compile(b"RetrieveDocumentSetRequest")
-
+reg_phr_service_endpoint = re.compile(
+    b'^(.*?<si:Service Name="PHRService">.*?<si:EndpointTLS Location="https://)(.*?)(/.*)$',
+    re.DOTALL,
+)
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "up and running 2"
 
-@app.route("/<path:path>", methods=["POST"])
+@app.route("/connector.sds", methods=["GET"])
+def connector_sds():
+    """resplace the endpoint für PHRService with our address"""
+    # <si:Service Name="PHRService">
+    # <si:EndpointTLS Location="https://kon-instanz1.titus.ti-dienste.de:443/soap-api/PHRService/1.3.0"/>
+    response = request_upstream()
+
+    m = reg_phr_service_endpoint.match(response.content)
+    if not m:
+        KeyError("connector.sds does not contain PHRService location.")
+
+    data = m.group(1) + bytes(request.host, "ASCII") + m.group(3)
+
+    return create_response(data)
+
+
+@app.route("/<path:path>", methods=ALL_METHODS)
 def soap(path):
-    # get data from upstream
-    upstream = requests.post(
-        f"{UPSTREAM_SERVER}{request.path}",
-        headers=request.headers,
-        data=request.get_data(),
-        cert=(PROXY_SSL_CERT, PROXY_SSL_KEY),
-        verify=SSL_VERIFY,
-    )
+    upstream = request_upstream()
 
     if reg_retrieve_document.search(request.get_data()):
-        response = Response(run_antivirus(upstream) or upstream.content)
+        data = run_antivirus(upstream) or upstream.content
     else:
-        response = Response(upstream.content)
+        data = upstream.content
+
+    return create_response(data)
+
+
+def request_upstream() -> Response:
+    remote_addr = request.remote_addr
+
+    if not config[remote_addr]:
+        raise KeyError(f"Client ${remote_addr} not found in av_gate.ini")
+
+    cfg = config[remote_addr]
+    konn = cfg["Konnektor"]
+    url = f"https://{konn}{request.path}"
+    data = request.get_data()
+    cert = None
+    if cfg.get("proxy_ssl_cert"):
+        cert = (cfg["proxy_ssl_cert"], cfg["proxy_ssl_key"])
+    verify = cert != None
+
+    response = requests.request(
+        method=request.method,
+        url=url,
+        headers=request.headers,
+        data=data,
+        cert=cert,
+        verify=verify,
+    )
+
+    if bytes(konn, "ASCII") in response.content:
+        app.logger.warn(f"Found Konnektor Address in response: {konn} - {request.path}")
+
+    return response
+
+
+def create_response(data) -> Response:
+    response = Response(data)
 
     # copy headers from upstream response
-    for k, v in upstream.headers.items():
+    for k, v in request.headers.items():
         if k not in ["Transfer-Encoding", "Content-Length"]:
             response.headers[k] = v
 
+    # overwrite content-length with current length
     response.headers["Content-Length"] = str(response.content_length)
 
     return response
 
+
 def run_antivirus(res: Response):
-    body = bytes(f"Content-Type: {res.headers['Content-Type']}\r\n\r\n\r\n", "ascii") + res.content
+    body = (
+        bytes(f"Content-Type: {res.headers['Content-Type']}\r\n\r\n\r\n", "ascii")
+        + res.content
+    )
     msg = email.parser.BytesParser(policy=email.policy.default).parsebytes(body)
     virus_found = 0
 
     for att in msg.iter_attachments():
-        scan_res = cd.instream(io.BytesIO(att.get_content()))["stream"]
+        scan_res = clamav.instream(io.BytesIO(att.get_content()))["stream"]
         content_id = att["Content-ID"]
         if scan_res[0] == "OK":
             app.logger.info(f"scanned ${content_id} : ${scan_res}")
@@ -64,7 +123,7 @@ def run_antivirus(res: Response):
             app.logger.info(f"virus found ${content_id} : ${scan_res}")
 
             # replace document
-            att.set_content(VIRUS_MSG)
+            att.set_content(config["config"]["virus_found"])
 
             # fix headers
             del att["MIME-Version"]
